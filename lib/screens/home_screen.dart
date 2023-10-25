2d363d639613ae24b7459c0f460213699b7e6b53
Jose_Maria <josemariazapataq2@gmail.com>
2023-10-24 22:58:06 -0400

    version 1.2

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,30 +1,69 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:practico_postips/screens/signin_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:practico_postips/style/app_style.dart';
+import 'package:practico_postips/widgets/note_card.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget{
+    const HomeScreen({Key? key}) : super(key: key);
 
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
+    @override
+    State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Logout"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut().then((value) {
-              print("Signed Out");
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SignInScreen()));
-            });
-          },
-        ),
-      ),
-    );
-  }
+class _HomeScreenState extends State<HomeScreen>{
+    @override
+    Widget build(BuildContext context){
+        return Scaffold(
+            backgroundColor: AppStyle.mainColor,
+            appBar: AppBar(
+                elevation: 0.0,
+                title: const Text("Mis Postips"),
+                centerTitle: true,
+                backgroundColor: AppStyle.mainColor,
+            ),
+            body: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                mainAxisAlignment:
+                    MainAxisAlignment.start,
+                crossAxisAlignment:
+                    CrossAxisAlignment.start,
+                children: [
+                    Text("Mis Postips Recientes", style: GoogleFonts.roboto(
+                        color:Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                            ),
+                        ),
+                        const SizedBox(
+                            height: 20.0,
+                        ),
+                        Expanded(
+                          child: StreamBuilder<QuerySnapshot>(
+                              stream: FirebaseFirestore.instance.collection("Notes").snapshots(),
+                              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot){
+                                  if(snapshot.connectionState == ConnectionState.waiting){
+                                      return  Center(
+                                          child: CircularProgressIndicator(),
+                                      );
+                                  }
+                                  if (snapshot.hasData){
+                                      return GridView(
+                                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                          crossAxisCount: 2),
+                                      children: snapshot.data!.docs.map((note) => noteCard(() {}, note)).toList()
+                                      );
+                                  }
+                              return Text ("No hay Postips",
+                                  style: GoogleFonts.nunito(
+                                      color: Colors.white),);
+                              },
+                          ),
+                        )
+                    ],
+                ),
+            ),
+        );
+    }
 }
---
+++ lib/widgets/note_card.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:practico_postips/style/app_style.dart';
+
+Widget noteCard(Function()? onTap, QueryDocumentSnapshot doc){
+        return InkWell(
+            onTap: onTap,
+            child:Container(
+                padding: EdgeInsets.all(8.0),
+                margin: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    color: AppStyle.cardsColor[doc["color"]],
+                    borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                        Text(doc["note_title"],style: AppStyle.mainTitle,),
+                        Text(doc["creation_date"],style: AppStyle.dateTitle,),
+                        Text(doc["note_content"],style: AppStyle.mainContent,),
+                    ],
+                ),
+            )
+        );
+}
+
+
